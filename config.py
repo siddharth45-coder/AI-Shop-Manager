7ed5756f3773a1311a0e3612bef4eca2fb8b695b
Siddharth Babu Ambat <siddharthambat18@hotmail.com>
2026-09-11 11:48:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLITECLOUD_CONNECTION = os.getenv("SQLITECLOUD_CONNECTION")
+
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "development-secret-key"
+    )
